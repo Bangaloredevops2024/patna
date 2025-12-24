ddcb9134dc789f6cdb9089fea01b526604e545e7
Pundeo Singh <pundeo2912@gmail.com>
2025-12-24 21:52:25 +0530

    code added

---
+++ environment/dev/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+      subscription_id = "ca2d764d-6af0-4ecc-8eaf-76bd3b666806"
+    }
+  # Configuration options
