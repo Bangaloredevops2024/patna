294aced8e3e504df01b86897df1ea4fc56a8a5b6
Pundeo Singh <pundeo2912@gmail.com>
2025-12-30 21:55:03 +0530

    code added

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -1,4 +1,29 @@
 module "rg_group" {
-  source = "../../module/azurerm_resource_group"
+  source         = "../../module/azurerm_resource_group"
   resource_group = var.resource_group
 }
+
+# module "nsg" {
+#   source = "../../module/networking"
+#   nsg    = var.nsg
+#   depends_on = [ module.rg_group ]
+# }
+
+module "vnet" {
+  source  = "../../module/azurerm_vnet"
+  vnet = var.vnet
+  depends_on = [ module.rg_group ]
+}
+
+module "subnets" {
+  source  = "../../module/subnet"
+  subnets = var.subnets
+  depends_on = [ module.vnet ]
+
+}
+
+module "public_ip" {
+  source = "../../module/public_ip"
+  public_ip = var.public_ip
+}
+
--- environment/dev/variable.tf
+++ environment/dev/variable.tf
@@ -1,8 +1,54 @@
 variable "resource_group" {
   description = "A map of resource group configurations."
   type = map(object({
-    name       = string
-    location   = string
-    tags       = optional(map(string))
+    name     = string
+    location = string
+    tags     = optional(map(string))
   }))
 }
+
+# variable "nsg" {
+#   type = map(object({
+#     name                = string
+#     location            = string
+#     resource_group_name = string
+#     virtual_network_name = string
+
+#   }))
+
+# }
+
+variable "vnet" {
+  type = map(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    address_space       = list(string)
+  }))
+}
+
+variable "subnets" {
+  type = map(object({
+    name                 = string
+    virtual_network_name = string
+    resource_group_name  = string
+    address_prefixes     = list(string)
+  }))
+}
+
+variable "public_ip" {
+  type = map(object({
+    name = string
+    location = string
+    resource_group_name = string
+    allocation_method = string
+  }))
+  
+}
+
+
+
+
+
+
+
